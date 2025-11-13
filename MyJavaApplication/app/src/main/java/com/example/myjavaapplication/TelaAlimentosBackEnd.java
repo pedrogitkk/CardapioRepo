8d558ba15ec2765a1802b176c615e25f3956290e
Pedro <pedro.tristao@etec.sp.gov.br>
2025-11-13 08:45:33 -0300

    Criado projeto com scroll view

---
+++ MyJavaApplication/app/src/main/java/com/example/myjavaapplication/TelaAlimentosBackEnd.java
@@ -0,0 +1,14 @@
+package com.example.myjavaapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TelaAlimentosBackEnd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_alimentos_front_end);
+    }
+}
